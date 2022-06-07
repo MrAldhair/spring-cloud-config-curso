931e01cd6d287e7358e36b30e13277892980e231
Aldhair Guadalupe Hernandez Soto <aldhair.hernandez@neoris.com>
2022-06-07 15:43:11 -0500

    eureka

--- motocicleta-servicio/src/main/java/com/motocicleta/servicio/MotocicletaServicioApplication.java
+++ motocicleta-servicio/src/main/java/com/motocicleta/servicio/MotocicletaServicioApplication.java
@@ -2,8 +2,10 @@ package com.motocicleta.servicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MotocicletaServicioApplication {
 
 	public static void main(String[] args) {
